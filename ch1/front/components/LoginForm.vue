60e30897e004ed01f455fbbd4255f33eb6176fdf
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-03-15 01:54:11 +0900

    -팔로워 팔로잉 수 확인 및 체크

--- ch1/front/components/LoginForm.vue
+++ ch1/front/components/LoginForm.vue
@@ -24,15 +24,17 @@
           <v-btn @click.prevent="onLogout">
             로그아웃
           </v-btn>
-          <v-col cols="4">
-            {{ me.Followings.length }} 팔로잉
-          </v-col>
-          <v-col cols="4">
-            {{ me.Followers.length }} 팔로워
-          </v-col>
-          <v-col cols="4">
-            {{ me.Posts.length }} 게시글
-          </v-col>
+          <v-row>
+            <v-col cols="4">
+              {{ me.Followings.length }} 팔로잉
+            </v-col>
+            <v-col cols="4">
+              {{ me.Followers.length }} 팔로워
+            </v-col>
+            <v-col cols="4">
+              {{ me.Posts.length }} 게시글
+            </v-col>
+          </v-row>
         </v-container>
       </v-card>
     </v-container>
